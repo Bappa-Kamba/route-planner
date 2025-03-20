d3b20e3a4b4caea0d60760f8d5530cea9c2fee35
Bappa-Kamba <bappahmuhammad18@gmail.com>
2025-03-20 11:31:47 +0100

    update SECRET_KEY in settings.py to use environment variable for enhanced security

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!q#upb=e--y9+qho2v=ng#^15t$_k#389avf10yyufsv54%u#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
